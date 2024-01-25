364d31bc3785a3628b459832c502f36c48401cb8
Dawiokkj <davilalongi@gmail.com>
2024-01-25 10:02:46 -0400

    alteração css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
     return (
     <>
-      <div>
+      <div className="card">
         <h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
