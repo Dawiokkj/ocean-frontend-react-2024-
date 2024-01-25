1be66364bb8f5c6d063befe8072a0ac15d2833e7
Dawiokkj <davilalongi@gmail.com>
2024-01-25 11:00:34 -0400

    Extração do componente card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
     return (
-    <>
-      <div className="card">
-        <h2 className='NameCard'>Rick Sanchez</h2>
-        <img className='ImgCard' src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>
-    </>
+      <>
+        <Card/>
+        <Card/>
+        <Card/>
+     </>
   )
 }
 
