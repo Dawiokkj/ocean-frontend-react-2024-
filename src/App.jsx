8cfdd983c2b383b03e5ddd8c42fa8a80814c1bcb
Dawiokkj <davilalongi@gmail.com>
2024-01-25 10:40:49 -0400

    Estilização completa do card

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
     return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+        <h2 className='NameCard'>Rick Sanchez</h2>
+        <img className='ImgCard' src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
     </>
   )
