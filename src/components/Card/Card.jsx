82ede85b746b5bac7616b71a5155953c58c62259
Dawiokkj <davilalongi@gmail.com>
2024-01-30 08:49:53 -0400

    Criação e Estilização de tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,15 @@ function Card() {
   return (
     <>
       <div className="card">
-        <h2 className='NameCard'>Rick Sanchez</h2>
-        <img className='ImgCard' src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+        <h2>Rick Sanchez</h2>
+        
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
+
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
     </>
   )
