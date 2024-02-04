865702000308fce6e9b4112d0db96592f69a48e0
Dawiokkj <davilalongi@gmail.com>
2024-02-04 02:27:27 -0400

    Alteração no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,7 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  // Carregamento de Dados direto via Codigo
-  // const item1 = {
-  //   name: 'Ricky sanchez',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  // }
-  // const item2 = {
-  //   name: 'Morty Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  // }
-  // const item3 = {
-  //   name: 'Summer Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  // }
-  // const item4 = {
-  //   name: 'Beth Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  // }
-
-  // const itens = [item1, item2, item3, item4]
-
-  const [itens, setItens] = useState([])
+    const [itens, setItens] = useState([])
 
   // Carregamento de Dados via API (Backend)
   async function carregarDadosAPI(){
